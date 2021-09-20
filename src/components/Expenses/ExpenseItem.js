b667e241dc843239a811d1c1a3dd7f01d66beacc
realdeeraj <55450569+realdeeraj@users.noreply.github.com>
2021-09-20 10:50:58 +0530

    $ to Rs

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -12,7 +12,7 @@ function ExpenseItem(props){
                 ></ExpenseDate>
                 <div className="expense-item__description">
                     <h2>{props.title}</h2> 
-                    <div className="expense-item__price">${props.amount}</div>
+                    <div className="expense-item__price">Rs {props.amount}</div>
                 </div>
             </Card> 
         </li>
